7ca9ff6f0adc72795bbf359c77982aa7417ca6d5
Aref <aref.behboudi@veltiston.ai>
2025-09-10 12:23:28 +0330

    Init Project

---
+++ src/test/java/com/embabel/embabel_translator_agent/TranslatorAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embabel.embabel_translator_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
